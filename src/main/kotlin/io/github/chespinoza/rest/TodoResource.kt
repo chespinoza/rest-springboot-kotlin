691b1afcae6ff61ccc1b57b4718e04438ddaf19c
christian espinoza <chespinoza@gmail.com>
2018-06-25 00:24:23 +0100

    adds basic resource

---
+++ src/main/kotlin/io/github/chespinoza/rest/TodoResource.kt
@@ -0,0 +1,25 @@
+package io.github.chespinoza.rest
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import java.time.Instant
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+
+interface TodoRepository: JpaRepository<Todo, Long>
+
+@RestController @RequestMapping(value = "/todo") @EnableWebMvc
+class TodoResource(val todoRepo: TodoRepository) {
+    @GetMapping(value = "/")
+    fun getAll() = todoRepo.findAll()
+}
+
+@Entity
+class Todo(@Id @GeneratedValue(strategy = GenerationType.AUTO)
+        val Id: Long = 0, var text: String = "", var done: Boolean = false, val createdAt: Instant = Instant.now())
