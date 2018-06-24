03ef90829bdce3f4daa49eff53bfd6642371dfe0
christian espinoza <chespinoza@gmail.com>
2018-06-25 00:06:29 +0100

    Initial commit

---
+++ src/main/kotlin/io/github/chespinoza/rest/RestApplication.kt
@@ -0,0 +1,11 @@
+package io.github.chespinoza.rest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestApplication>(*args)
+}
