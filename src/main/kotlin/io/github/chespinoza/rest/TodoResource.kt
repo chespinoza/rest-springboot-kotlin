12305fc0e1b3e3c0ec8a812a148bb607eff4f1ed
christian espinoza <chespinoza@gmail.com>
2018-06-25 01:25:19 +0100

    adds rest endpoints

--- src/main/kotlin/io/github/chespinoza/rest/TodoResource.kt
+++ src/main/kotlin/io/github/chespinoza/rest/TodoResource.kt
@@ -1,9 +1,7 @@
 package io.github.chespinoza.rest
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import java.time.Instant
 import javax.persistence.Entity
@@ -18,6 +16,23 @@ interface TodoRepository: JpaRepository<Todo, Long>
 class TodoResource(val todoRepo: TodoRepository) {
     @GetMapping(value = ["/"])
     fun getAll() = todoRepo.findAll()
+
+    @GetMapping(value = ["/{id}"])
+    fun getOne(@PathVariable id: Long) = todoRepo.findById(id)
+
+    @PostMapping(value = ["/"])
+    fun new(@RequestBody text: String) = todoRepo.save(Todo(text = text))
+
+    @DeleteMapping(value = ["/{id}"])
+    fun delete(@PathVariable id: Long) = todoRepo.deleteById(id)
+
+    @PutMapping(value = ["/{id}"])
+    fun update(@PathVariable id: Long, @RequestBody todo: Todo): Todo {
+        val toUpdate: Todo = todoRepo.findById(id).orElseThrow { Exception("error updating") }
+        toUpdate.text = todo.text
+        toUpdate.done = todo.done
+        return todoRepo.save(toUpdate)
+    }
 }
 
 @Entity
